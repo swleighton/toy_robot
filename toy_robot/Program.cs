91c88f62368a4fe545bfc7f62640b459aa423f9d
Simon Leighton <swleighton@live.co.uk>
2019-05-03 10:41:45 +1000

    Initial commit of base project

---
+++ toy_robot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace toy_robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
